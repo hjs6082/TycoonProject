3c1c985e76cec6c548ed450ecf8b37432385e515
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2022-05-16 20:13:57 +0900

    도감 기능 추가

---
+++ Assets/01.Scripts/Setting.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class Setting : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] AudioSource clickAudio;
+    [SerializeField] private GameObject cameraIcon;
+    [SerializeField] private GameObject settingIcon;
+    private bool isOpen;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isOpen)
+        {
+            cameraIcon.SetActive(true);
+            settingIcon.SetActive(true);
+            cameraIcon.transform.DOLocalMoveY(0f, 0.6f);
+            ClickSound();
+            settingIcon.transform.DOLocalMoveY(-100f, 0.6f).OnComplete(() => { isOpen = true; });
+        }
+        else if(isOpen)
+        {
+            ClickSound();
+            cameraIcon.transform.DOLocalMoveY(90f, 0.6f);
+            settingIcon.transform.DOLocalMoveY(90f, 0.6f).OnComplete(() => { EndTween(); });
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndTween()
+    {
+        isOpen = false;
+        cameraIcon.SetActive(false);
+        settingIcon.SetActive(false);
+    }
+
+    public void ClickSound()
+    {
+        clickAudio.Play();
+    }
+}
