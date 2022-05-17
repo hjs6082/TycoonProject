b87aafaa4a5c677a03b384d741b96ffd3e813129
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2022-05-17 21:03:11 +0900

    자동으로 캐주는 빌딩 제작완료

--- Assets/01.Scripts/AutoBuildingScript.cs
+++ Assets/01.Scripts/AutoBuildingScript.cs
@@ -1,66 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening;
 
-public class BuildingScript : MonoBehaviour, IPointerClickHandler
+public class AutoBuildingScript : MonoBehaviour
 {
     [SerializeField]
     private Building building;
-
-    //public int myMoney;
 
     public int nowMoney;
 
     public Text myMoneyText;
     public Text nowMoneyText;
+    private bool isRestart;
+
+    public GameObject moneyPlusPanel;
+
+    public Vector2 startPosition;
 
     private void OnEnable()
     {
         StartCoroutine(PlusMoney(1));
+        moneyPlusPanel = gameObject.transform.GetChild(0).gameObject;
+        startPosition = gameObject.transform.position;
+        nowMoneyText = gameObject.transform.GetChild(0).GetChild(0).GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         myMoneyText.text = UIManager.instance.myMoney.ToString();
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (nowMoney != 0)
+        switch (building.BuildingType)  
         {
-            AudioManager.instnace.PlusMoneySound();
-            nowMoneyText.text = "+" + nowMoney.ToString();
-            UIManager.instance.PlusMoney();
-            UIManager.instance.myMoney += nowMoney;   
-            switch (building.BuildingType)
-            {
-                case Building.buildingType.House:
+            case Building.buildingType.House:
+                if (isRestart == false)
+                {
                     if (nowMoney == 300)
                     {
+                        nowMoneyText.text = "+300";
+                        GetMoney();
+                        UIManager.instance.myMoney += nowMoney;
+                        nowMoney = 0;
                         StartCoroutine(PlusMoney(1));
+                        isRestart = true;
                     }
-                    break;
-                case Building.buildingType.Building:
+                }
+                break;
+            case Building.buildingType.Building:
+                if (isRestart == false)
+                {
                     if (nowMoney == 500)
                     {
+                        nowMoneyText.text = "+500";
+                        GetMoney();
+                        UIManager.instance.myMoney += nowMoney;
+                        nowMoney = 0;
                         StartCoroutine(PlusMoney(1));
+                        isRestart = true;
                     }
-                    break;
-                case Building.buildingType.Hotel:
+                }
+                break;
+            case Building.buildingType.Hotel:
+                if (isRestart == false)
+                {
                     if (nowMoney == 1000)
                     {
+                        nowMoneyText.text = "+1000";
+                        GetMoney();
+                        UIManager.instance.myMoney += nowMoney;
+                        nowMoney = 0;
                         StartCoroutine(PlusMoney(1));
+                        isRestart = true;
                     }
-                    break;
-                default:
-                    break;
-            }
-
-            nowMoney = 0;
+                }
+                break;
+            default:
+                break;
         }
+    }
+
+    public void GetMoney()
+    {
+        AudioManager.instnace.PlusMoneySound();
+        moneyPlusPanel.SetActive(true);
+        moneyPlusPanel.transform.DOLocalMoveY(320f, 0.5f).OnComplete(() => { moneyPlusPanel.SetActive(false); });
+        moneyPlusPanel.transform.localPosition = startPosition;
     }
 
     IEnumerator PlusMoney(float delayTime)
@@ -77,6 +101,7 @@ public class BuildingScript : MonoBehaviour, IPointerClickHandler
                 else if (nowMoney >= 300)
                 {
                     StopCoroutine(PlusMoney(1));
+                    isRestart = false;
                 }
                 break;
             case Building.buildingType.Building:
@@ -87,6 +112,7 @@ public class BuildingScript : MonoBehaviour, IPointerClickHandler
                 else if (nowMoney >= 500)
                 {
                     StopCoroutine(PlusMoney(1));
+                    isRestart = false;
                 }
                 break;
             case Building.buildingType.Hotel:
@@ -97,6 +123,7 @@ public class BuildingScript : MonoBehaviour, IPointerClickHandler
                 else if (nowMoney >= 1000)
                 {
                     StopCoroutine(PlusMoney(1));
+                    isRestart = false;
                 }
                 break;
             default:
@@ -104,6 +131,4 @@ public class BuildingScript : MonoBehaviour, IPointerClickHandler
         }
 
     }
-
-
 }
